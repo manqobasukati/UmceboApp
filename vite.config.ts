08668fcec61caf4d1ee4cb2bb9eb6486bf68578c
manqobasukati <manqoba.sukati@peoplesized.com>
2023-03-07 18:37:01 +0200

    LETS SEE

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,37 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Umcebo Money',
+        short_name: 'Umcebo',
+        description: 'The web builder app',
+        theme_color: '#81CFC1',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    }),
+  ],
   server: {
     host: '0.0.0.0',
   },
